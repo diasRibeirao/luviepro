1944e1fc77424f71d99f4f028a9a977d14122f3f
Emerson Dias <emerson@bmgtecnologia.com.br>
2026-09-05 04:57:07 -0300

    chore: remove obsolete untracked migrations

--- backend/prisma/migrations/20260904104831/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "Order_tenantId_createdAt_idx";
-
--- AlterTable
-ALTER TABLE "Order" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "ProductUnit" ALTER COLUMN "updatedAt" DROP DEFAULT;
--- backend/prisma/migrations/20260904124500_casa_nova_catalog_bulk/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "CasaNovaList" ADD COLUMN "defaultsInitialized" BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE "CasaNovaItem" ADD COLUMN "quantityOverride" INTEGER;
